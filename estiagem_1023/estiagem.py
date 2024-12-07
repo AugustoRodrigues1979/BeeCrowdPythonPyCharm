0aa38d8709949f04bd3103ce346f36b8f7634b74
AugustoRodrigues1979 <guto.rodrigues1979@gmail.com>
2024-12-07 11:23:14 -0300

    Last Try implemented but dont acceptable by budge :-(

--- estiagem_1023/estiagem.py
+++ estiagem_1023/estiagem.py
@@ -23,6 +23,9 @@
 #
 import math
 
+def callback_for_item_default(result, value):
+    result.append(value)
+
 def create_node(value):
     return {'value': value, 'left': None, 'right': None}
 
@@ -44,10 +47,11 @@ def insert(root, value):
             current = current['right']
 
 def balance_tree(root):
-    inorder_values = inorder(root)
+    callback = callback_for_item_default
+    inorder_values = inorder(root,callback)
     return sorted_array_to_bst(inorder_values)
 
-def inorder(root):
+def inorder(root, callback_for_item):
     stack = []
     result = []
     current = root
@@ -56,7 +60,7 @@ def inorder(root):
             stack.append(current)
             current = current['left']
         current = stack.pop()
-        result.append(current['value'])
+        callback_for_item(result, current['value'])
         current = current['right']
     return result
 
@@ -72,8 +76,8 @@ def sorted_array_to_bst(arr):
         return root
     return insert_level_order(arr, None, 0, len(arr))
 
-def get_list_inorder(root):
-    return inorder(root)
+def get_list_inorder(root, callback_for_item=callback_for_item_default):
+    return inorder(root, callback_for_item)
 
 def round_down(number, decimal_places):
     factor = 10 ** decimal_places
@@ -86,52 +90,67 @@ def collect_info_house():
     average_consume_by_person = amount_consumed_family // amount_residents
     return average_consume_by_person, amount_residents, amount_consumed_family
 
-def mount_second_line(root):
-    consumption_list = get_list_inorder(root)
-    output_str = ''
-    total_residents = 0
-    previous_consumption = -1
-    for consumption_value in consumption_list:
-        average_consumption = consumption_value // 1000
-        residents = consumption_value % 1000
-        if previous_consumption == -1:
-            previous_consumption = average_consumption
-        if previous_consumption != average_consumption:
-            output_str += f"{total_residents}-{previous_consumption} "
-            total_residents = residents
-            previous_consumption = average_consumption
-        else:
-            total_residents += residents
-    output_str += f"{total_residents}-{previous_consumption}"
-    return output_str
+def mount_second_line(root, average_consumption_city):
+    def treatment_by_consumption(consumption_array, key_consumption):
+        key_consumption = key_consumption // 1000
+        key_consumption_str = str(key_consumption)
+        if key_consumption_str in average_consumption_city:
+            amount_residents = average_consumption_city.pop(key_consumption_str)
+            treatment_by_consumption.output_str += f"{amount_residents}-{key_consumption_str}"
+            if len(average_consumption_city) > 0:
+                treatment_by_consumption.output_str += " "
 
-def run_challenge():
-    info_city_str = ''
-    exist_at_least_one_city = False
-    id_city = 0
+    treatment_by_consumption.output_str = ''
+    get_list_inorder(root, treatment_by_consumption)
+    list_a = [4,5,6,7,22,1,-3,4]
+    list_a.sort()
+    return treatment_by_consumption.output_str
+
+def collect_data_city():
+    amount_cities = 0
+    cities_dict = dict()
     while True:
         amount_house = int(input())
         if amount_house == 0:
             break
-        id_city += 1
-        output_first_line = f'Cidade# {id_city}:'
+        amount_cities += 1
         total_persons = 0
         total_amount_consume = 0
         root = None
+        average_consumption_by_house = dict()
         for home_index in range(amount_house):  # For each home
             average_consume_by_person, amount_residents, amount_consumed_family  = collect_info_house()
             root = insert(root, average_consume_by_person * 1000 + amount_residents)
             total_persons += amount_residents
             total_amount_consume += amount_consumed_family
-        output_second_line = mount_second_line(root)
-        average_consume_by_city = total_amount_consume / total_persons
+            key_average_consumption = str(average_consume_by_person)
+            if key_average_consumption in average_consumption_by_house:
+                average_consumption_by_house[key_average_consumption] += amount_residents
+            else:
+                average_consumption_by_house[key_average_consumption] = amount_residents
+
+        cities_dict[f'K_CITY_'+str(amount_cities)] = [root, total_persons, total_amount_consume, average_consumption_by_house]
+    return amount_cities, cities_dict
+
+def run_challenge():
+    info_city_str = ''
+    amount_cities, cities_dict = collect_data_city()
+    if amount_cities == 0:
+        return
+    id_city = 0
+    for key_city in cities_dict:
+        info_city_lst = cities_dict[key_city]
+        output_first_line = f'Cidade# {id_city+1}:'
+        output_second_line = mount_second_line(info_city_lst[0], info_city_lst[3])
+        average_consume_by_city = info_city_lst[2] / info_city_lst[1]
         average_consume_by_city = round_down(average_consume_by_city, 2)
         output_third_line = f'Consumo medio: {average_consume_by_city:.02f} m3.'
         info_city_str += output_first_line + '\n' + output_second_line + '\n' + output_third_line
-        info_city_str += '\n'
-        exist_at_least_one_city = True
-    if exist_at_least_one_city:
-        print(info_city_str[:-1], end='')
+        if id_city < amount_cities - 1:
+            info_city_str += '\n'
+        id_city += 1
+
+    print(info_city_str, end='')
 
 if __name__ == '__main__':
     run_challenge()
