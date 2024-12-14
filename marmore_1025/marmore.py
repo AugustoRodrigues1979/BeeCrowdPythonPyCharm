2e2e095be1f99014193a07d16f1e6bd5ceaab861
AugustoRodrigues1979 <guto.rodrigues1979@gmail.com>
2024-12-14 19:04:59 -0300

    Fixed algorithm for reply challenge marmore 1025

--- marmore_1025/marmore.py
+++ marmore_1025/marmore.py
@@ -1,6 +1,11 @@
 from sys import stdin, stdout
 
 
+def print_with_flush(output_str):
+    stdout.write(output_str)
+    stdout.flush()
+
+
 def run_challenge():
     result_dict = dict()
     id_serie_use_case = 0
@@ -24,23 +29,30 @@ def run_challenge():
                 marble_dict[marble_key] = i
 
         query_result_dict = dict()
+        request_query_list = []
         for i in range(amount_queries):
             query_number = str(int(input()))
             if query_number in marble_dict:
                 query_result_dict[query_number] = f'{int(query_number)} found at {marble_dict[query_number]}'
             else:
                 query_result_dict[query_number] = f'{int(query_number)} not found'
+            request_query_list.append(query_number)
 
         id_serie_use_case += 1
-        result_dict[str(id_serie_use_case)] = query_result_dict
+        result_dict[str(id_serie_use_case)] = dict()
+        result_dict[str(id_serie_use_case)]["data"] = query_result_dict
+        result_dict[str(id_serie_use_case)]["queries_id"] = request_query_list
+
 
     id_serie_use_case = 1
     for key_result in result_dict.keys():
         print(f'CASE# {id_serie_use_case}:\n')
         id_serie_use_case += 1
         query_result_dict = result_dict[key_result]
-        for query_key in query_result_dict:
-            result_str = query_result_dict[query_key]
+        request_query_list = query_result_dict['queries_id']
+        for i in range(len(request_query_list)):
+            query_key = str(request_query_list[i])
+            result_str = query_result_dict['data'][query_key]
             print(f'{result_str}\n')
 
 
