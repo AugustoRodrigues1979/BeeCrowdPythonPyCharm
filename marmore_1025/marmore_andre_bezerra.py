c54fb61ecb5ec99cfdba19fb410ed6f891e3c074
AugustoRodrigues1979 <guto.rodrigues1979@gmail.com>
2024-12-15 09:39:40 -0300

    Restoring original code of algorithm provided by user of BeeCrowd Site

--- marmore_1025/marmore_andre_bezerra.py
+++ marmore_1025/marmore_andre_bezerra.py
@@ -1,15 +1,15 @@
-def pesquisa_binaria(V, valor):
+def pesquisaBinaria(V, valor):
     inicio, fim = 0, len(V)
 
-    while inicio < fim:
+    while(inicio < fim):
         meio = (inicio + fim) // 2
 
-        if V[meio] < valor:
+        if(V[meio] < valor):
             inicio = meio + 1
         else:
             fim = meio
 
-    if inicio >= len(V):
+    if(inicio >= len(V)):
         return -1
 
     return inicio + 1 if V[inicio] == valor else -1
@@ -18,26 +18,29 @@ def pesquisa_binaria(V, valor):
 def run_challenge():
     T = 1
     while True:
-        N, Q = list(map(int, (input()).split()))
+        try:
+            N, Q = list(map(int, (input()).split()))
 
-        if N == 0 and Q == 0:
+            if N == 0 and Q == 0:
+                break
+
+            marmores = []
+            for _ in range(N):
+                marmores.append(int(input()))
+            marmores.sort()
+
+            print(f'CASE# {T}:')
+            T += 1
+            for _ in range(Q):
+                consulta = int(input())
+
+                resposta = pesquisaBinaria(marmores, consulta)
+                if resposta == -1:
+                    print(f'{consulta} not found')
+                else:
+                    print(f'{consulta} found at {resposta}')
+        except EOFError:
             break
-
-        marmores = []
-        for _ in range(N):
-            marmores.append(int(input()))
-        marmores.sort()
-
-        print(f'CASE# {T}:', end='\n')
-        T += 1
-        for _ in range(Q):
-            consulta = int(input())
-
-            resposta = pesquisa_binaria(marmores, consulta)
-            if resposta == -1:
-                print(f'{consulta} not found', end='\n')
-            else:
-                print(f'{consulta} found at {resposta}', end='\n')
 
 if __name__ == '__main__':
     run_challenge()
