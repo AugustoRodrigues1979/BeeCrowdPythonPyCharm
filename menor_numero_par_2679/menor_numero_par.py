fd5c7c864c8ea61ad00990f412724a1d0a7e0b35
AugustoRodrigues1979 <guto.rodrigues1979@gmail.com>
2024-12-16 10:24:32 -0300

    fixed the algorithm and yours unit test

--- menor_numero_par_2679/menor_numero_par.py
+++ menor_numero_par_2679/menor_numero_par.py
@@ -3,13 +3,11 @@ from sys import stdin, stdout
 def run_challenge():
     number = int(input())
     if number % 2 == 0:
-        number *= 2
+        number += 2
     else:
         number += 1
 
-    print(str(number))
+    print(str(number), end='\n')
 
 if __name__ == '__main__':
-    input = stdin.readline
-    print = stdout.write
     run_challenge()
--- test/test_menor_numero_par_2679.py
+++ test/test_menor_numero_par_2679.py
@@ -4,8 +4,8 @@ from menor_numero_par_2679.menor_numero_par import run_challenge
 
 class MyTestCase(unittest.TestCase):
     def test_challenge_with_valid_input_beewcrod(self):
-        mock_inputs = iter(['1'])
-        expected_output = '2'
+        mock_inputs = iter(['222'])
+        expected_output = '224'
         with patch('builtins.print') as mock_print:  # Mock print function
             with patch('builtins.input', lambda: next(mock_inputs)):  # Mock input function
                 run_challenge()  # Run Challenge
